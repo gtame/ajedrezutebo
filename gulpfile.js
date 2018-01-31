97f81732ae689e257320f8f7da41286de9aeac0b
gtame <Gabriel Tame>
2018-01-31 19:39:03 +0100

    Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var pkg = require('./package.json');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var cssnano = require('gulp-cssnano');
+var gulpCopy = require('gulp-copy');
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function() {
@@ -39,3 +45,65 @@ gulp.task('dev', ['browserSync'], function() {
   gulp.watch('./css/*.css', browserSync.reload);
   gulp.watch('./*.html', browserSync.reload);
 });
+
+
+
+ 
+gulp.task('compressHtml',function()
+{
+  return gulp.src('index_prod.html')
+  .pipe(htmlmin({collapseWhitespace: true,removeComments: true}))
+  .pipe(gulp.dest('dist'));
+
+});
+ 
+gulp.task('compressCSS',function()
+{
+  gulp.src(['./css/*.css','vendor/bootstrap/css/bootstrap.min.css'])
+  .pipe(concat('all.css'))
+  .pipe(cssnano(  {"discardComments": {"removeAll": true}}))
+  .pipe(gulp.dest('dist')
+);
+});
+
+//compress
+gulp.task('compressJS', function () {
+
+        //js
+        gulp.src(['./assets/js/*.js','js/main.js','./vendor/countdowntime/countdowntime.js','./vendor/bootstrap/js/bootstrap.bundle.min.js','vendor/jquery/jquery.min.js'])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist')
+        //css
+
+
+
+  );
+});
+ 
+
+
+//compressAll
+gulp.task('compress', function () {
+
+  //js
+  gulp.start('compressJS');
+  //css
+  gulp.start('compressCSS');
+  //HTML
+  gulp.start('compressHtml');
+
+
+  gulp.src(['assets/img/*.png'])
+  .pipe(gulpCopy('dist/'));
+
+  gulp.src(['images/*'])
+  .pipe(gulpCopy('dist/'));
+
+
+  gulp.src(['images/icons/*'])
+  .pipe(gulpCopy('dist/'));
+
+
+});
+
