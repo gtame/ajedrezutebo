7c8ed5a3fef36cb8b61a9abed6664d9630e69cb9
gtame <Gabriel Tame>
2018-01-31 17:50:05 +0100

    Fondo gris + Google maps Working

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,45 @@ if (show) {
 rotatetext();
 
 
-
+var map =null;
+  var panorama=null;
+  
+    //Gogle maps
+    function initMap() {
+      var fenway = {lat: 41.711547, lng: -1.00189};
+      
+          
+      var marker = new google.maps.Marker({
+        position: fenway,
+        title:"Pabellon"});
+        
+        map = new google.maps.Map(document.getElementById('mapa'), {
+          center: fenway,
+          zoom: 14
+        });
+        panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), {
+              position: fenway,
+              pov: {
+                heading: 34,
+                pitch: 8
+              }
+            });
+        marker.setMap(map);	
+  
+        map.setStreetView(panorama);
+  
+  
+  
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(fenway);
+  
+        google.maps.event.trigger(panorama, 'resize');
+        map.setCenter(fenway);
+        
+     }	
+  
+  
 showdiv= function(mydiv) {
 
    $("#home").hide(); 
@@ -57,6 +95,8 @@ $("#nvhome").click(function()
 
 $("#nvmap").click(function ()
 {
+    if (map==null)
+        initMap();
     showdiv($("#mapa"));
     $("#pano").show();
 
